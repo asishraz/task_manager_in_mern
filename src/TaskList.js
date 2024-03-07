19bcf92561d31925b2f16d4e27bd38356617a8b9
asishraz <iamasish3001@gmail.com>
2024-03-07 23:29:02 +0530

    more dependency added

---
+++ src/TaskList.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+const TaskList = () => {
+    const [tasks, setTasks] = useState([]);
+
+    return (
+        <div> 
+            <h2> TaskList </h2>
+            {/* display tasks here */}
+        </div>
+    )
+
+}
+
+export defualt TaskList;
